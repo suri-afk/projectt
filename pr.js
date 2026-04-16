30d59ba614942b817b2a7fd07b71ca82ec9df889
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2026-04-16 18:46:37 +0300

    FF
    FF

--- pr.js
+++ pr.js
@@ -5,3 +5,23 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+$("body").on("keyup", "#filter-input", function() {
+  var searchText =
+      $("#filter-input")
+  .val()
+  .toLowerCase() || "___";
+  $("#container > p").each(function(i) {
+    var elem = $(this);
+    if (
+      elem
+      .html()
+      .toLowerCase()
+      .indexOf(searchText) === -1
+    ) {
+      elem.addClass("hidden");
+    } else {
+      elem.removeClass("hidden");
+    }
+  });
+});
